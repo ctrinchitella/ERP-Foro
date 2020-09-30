440346ef1271156c8603694e57ef5cb1cfdec95a
ctrinchitella <carla.trinchitella@gmail.com>
2020-09-30 15:57:26 -0300

    Alpha 1.6 - scroll and connection with the rest of the page

--- src/App.js
+++ src/App.js
@@ -1,81 +1,26 @@
 //import React from 'react';
 import React, { Component, useRef } from 'react';
 import PropTypes from 'prop-types';
-import { Button } from '@material-ui/core';
 import HeaderBar from './HeaderBar.js';
 import Carrousel from './Carrousel.js';
 import Docbtn from './DocumentBtn.js';
 import QA from './QA.js';
 import Issues from './Issues.js';
 import Add from './add.js';
-import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
 import './App.css';
-import { BorderStyle } from '@material-ui/icons';
-
-/*class Text extends Component{
-  render(){
-    const {arrayofNumbers,
-           number,
-           objectValues,
-           text} = this.props
-    return (
-    <div>
-      <p>{arrayofNumbers.join(', ')}</p>
-      <p>{objectValues.key2}</p>
-    </div>
-    )
-  
-  }
-}*/
-
-/*function TabPanel(props) {
-  const { children, value, index, ...other } = props;
-
-  return (
-    <div
-      role="tabpanel"
-      hidden={value !== index}
-      id={`scrollable-auto-tabpanel-${index}`}
-      aria-labelledby={`scrollable-auto-tab-${index}`}
-      {...other}
-    >
-      {value === index && (
-        <Box p={3}>
-          <Typography>{children}</Typography>
-        </Box>
-      )}
-    </div>
-  );
-}
-
-TabPanel.propTypes = {
-  children: PropTypes.node,
-  index: PropTypes.any.isRequired,
-  value: PropTypes.any.isRequired,
-};*/
-
-class ReadyToScroll extends Component {
-
-  constructor(props) {
-      super(props)
-      this.myRef = React.createRef()  
-  }
-
-  render() {
-      return <div ref={this.myRef}></div> 
-  }  
-
-  scrollToMyRef = () => window.scrollTo(0, this.myRef.current.offsetTop)   
-  // run this method to execute scrolling. 
-
-}
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import { grey } from '@material-ui/core/colors';
 
 function App() {
+  
   return (
     <view style={{ flex: 1, scrollBehavior: "smooth" }}>
       <div id="Home">
         <div className='App'>
+        
             <HeaderBar></HeaderBar>
             <Carrousel></Carrousel>
             <div className='ButtonContainer' //style={
--- src/HeaderBar.js
+++ src/HeaderBar.js
@@ -3,51 +3,8 @@ import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
-import red from '@material-ui/core/colors/red';
 import { grey } from '@material-ui/core/colors';
-import { animateScroll as scroll} from 'react-scroll';
-import deleteIcon from '@material-ui/icons/Delete';
-import Icon from '@material-ui/core/Icon';
-import { Link, animateScroll } from "react-scroll";
 
-  
-const ButtonsBar = {
-    width: '130px',
-    height: '100%',
-    fontSize: '20px',
-    font: 'Arial',
-    color: 'white',
-    paddingTop: '10px',
-    paddingBottom: '15px'
-}
-
-
-
-class Buttons extends Component {
-    render(){
-        const {title} = this.props
-        return (
-        <div style={ButtonsBar}>{title}</div>
-        )
-    }
-}
-
-
-
-/*export default class HeaderBar extends Component{
-    render(){
-        const primary = grey[900];
-        return(
-            <AppBar color={primary} position="static">
-                <Tabs aria-label="simple tabs example">
-                    <Tab label="Item One"  />
-                    <Tab label="Item Two"  />
-                    <Tab label="Item Three" />
-                </Tabs>
-            </AppBar>
-        );
-    }
-}*/
 const useStyles = makeStyles({
     root: {
         background: 'black',
@@ -81,88 +38,29 @@ function a11yProps(index) {
     };
   }
 
+function LinkTab(props) {
+    return <Tab component="a" {...props} />;
+}
+
 export default function HeaderBar() {
     const classes = useStyles();
     const [value, setValue] = React.useState(0);
 
     const handleChange = (event, newValue) => {
         setValue(newValue);
+        console.log(value);
     };
-
-    const handleChangeIndex = (index) => {
-        setValue(index);
-    };
-        return(
-        <AppBar className={classes.appBarStyle} position="static">
-            <Tabs 
-            value={value}
-            onChange={handleChange}
-            indicatorColor="primary" classes={{indicator: classes.indicator}}>
-                <Tab label="Netsuite"  {...a11yProps(0)}/>
-                <Tab label="JD Edwards" {...a11yProps(1)} />
-                <Tab label="SAP" {...a11yProps(2)}/>
-                <Tab label="Oracle Cloud" {...a11yProps(3)}/>
-            </Tabs>
-        </AppBar>
-        );
-    
-}   
-/*<div style={HeaderStyle}>
-            <a onClick={(e) => {console.log('Scrolling to NS');}}>
-                <Buttons title='Netsuite'/>
-            </a>
-            <a onClick={(e) => {console.log('Scrolling to JDE');}}>
-                <Buttons title='JD Edwards'/>
-            </a>
-            <a onClick={(e) => {console.log('Scrolling to SAP');}}>
-                <Buttons title='SAP'/>
-            </a>
-            <a onClick={(e) => {console.log('Scrolling to OC');}}>
-                <Buttons title='Oracle Cloud'/>
-            </a>
-        </div> */
-
-
-/*import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
-
-const useStyles = makeStyles({
-  root: {
-    flexGrow: 1,
-  },
-});
-
-export default function CenteredTabs() {
-  const classes = useStyles();
-  const [value, setValue] = React.useState(0);
-
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
-  };
-
-  return (
-    <Paper className={classes.root}>
-      <Tabs
+    return( 
+    <AppBar className={classes.appBarStyle} position="static">
+        <Tabs 
         value={value}
         onChange={handleChange}
-        indicatorColor="primary"
-        textColor="primary"
-        centered
-      >
-        <Tab label="Item One" />
-        <Tab label="Item Two" />
-        <Tab label="Item Three" />
-      </Tabs>
-    </Paper>
-  );
-} */
-
-/*<div style={HeaderStyle}>
-    <Buttons title='Netsuite'/>
-    <Buttons title='JD Edwards'/>
-    <Buttons title='SAP'/> 
-    <Buttons title='Oracle Cloud'/>
-</div>*/ 
+        indicatorColor="primary" classes={{indicator: classes.indicator}}>
+            <LinkTab label="Netsuite" {...a11yProps(1)} href="#Netsuite"/>
+            <LinkTab label="JD Edwards" {...a11yProps(1)} href="#JDEdwards"/>
+            <LinkTab label="SAP" {...a11yProps(2)} href="#SAP"/>
+            <LinkTab label="Oracle Cloud" {...a11yProps(3)} href="#OracleCloud"/>
+        </Tabs>
+    </AppBar>
+    );
+}   
